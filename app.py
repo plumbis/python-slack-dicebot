140ce2f397165d6727c297b835fc5777462b2eee
Pete Lumbis <plumbis@cumulusnetworks.com>
2016-12-05 22:26:10 -0500

    update to POST method support

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route("/test")
 def roll():
     return "Hello World!"
 
-@app.route('/<name>')
+
+@app.route('/<name>', methods=["GET", "POST"])
 def hello_name(name):
     return "Hello {}!".format(name)
+
 
 if __name__ == "__main__":
     app.run()
