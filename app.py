84f19627f06fb4f6c4f890ce7c8da6dd6a8d84ea
Pete Lumbis <plumbis@cumulusnetworks.com>
2016-12-05 22:20:43 -0500

    removing slack-flask library

--- app.py
+++ app.py
@@ -1,23 +1,13 @@
-# from flask import Flask
-# app = Flask(__name__)
+from flask import Flask
+app = Flask(__name__)
 
-# @app.route("/test")
-# def roll():
-#     return "Hello World!"
+@app.route("/test")
+def roll():
+    return "Hello World!"
 
-# @app.route('/<name>')
-# def hello_name(name):
-#     return "Hello {}!".format(name)
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
 
-# if __name__ == "__main__":
-#     app.run()
-
-from flask_slack import Slack
-
-slack = Slack(app)
-
-@slack.command('test', token="not_required", methods=['POST'])
-
-def your_method(**kwargs):
-    text = kwargs.get('text')
-    return slack.response(text)
+if __name__ == "__main__":
+    app.run()
