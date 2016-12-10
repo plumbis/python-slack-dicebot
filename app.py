84611878eb666cbaf57b28cb767322d58c196e56
Pete Lumbis <plumbis@cumulusnetworks.com>
2016-12-09 23:01:44 -0500

    correct string call

--- app.py
+++ app.py
@@ -352,7 +352,7 @@ def format_character_roll(roll_list, username):
     for roll in roll_list:
         try:
             sorted_rolls = sorted(roll["rolls"], key=int)
-            output_text.append("(" + str(sorted_rolls[0] + ") "))
+            output_text.append("(" + str(sorted_rolls[0]) + ") ")
             output_text.append(str(sorted_rolls[1]) + " + ")
             output_text.append(str(sorted_rolls[2]) + " + ")
             output_text.append(str(sorted_rolls[3]) + " = ")
